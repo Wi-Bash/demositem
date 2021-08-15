580454e5ac778e66dd735c14668dda3c1691add8
blacol <anthonycolvil@gmail.com>
2021-08-15 06:19:30 +0200

    implémentation sliders

---
+++ src/components/Slideshow.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image'
+import img1 from '../assets/img1.jpg'
+import img2 from '../assets/img2.jpg'
+import img3 from '../assets/img3.jpg'
+
+
+
+const proprietes = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true
+}
+
+const Slideshow = () => {
+    return (
+        <div className="containerSlide">
+            <Slide {...proprietes}>
+                <div className="each-slide">
+                    <div>
+                        <img src={img1} alt="img1" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src={img2} alt="img2" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src={img3} alt="img3" />
+                    </div>
+                </div>
+            </Slide>
+        </div>
+    )
+}
+
+export default Slideshow;
