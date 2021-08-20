d599cd208686a538f254c5f50ac1df5809feb5d9
blacol <anthonycolvil@gmail.com>
2021-08-20 12:31:33 +0200

    maj nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,9 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <NavLink exact to="/" activeClassName="nav-active">Accueil</NavLink>
+            <NavLink exact to="/Maville" activeClassName="nav-active">Ma ville</NavLink>
+            <NavLink exact to="/servicepopulation" activeClassName="nav-active">Service population</NavLink>
+            <NavLink exact to="/Demarche" activeClassName="nav-active">Demarche</NavLink>
             <NavLink exact to="/About" activeClassName="nav-active">About</NavLink>
             
         </div>
--- src/components/pub.js
+++ src/components/pub.js
@@ -3,11 +3,54 @@ import React from 'react';
 const Pub = () => {
     return (
         <div className="containerpub">
-            <h1>Pub</h1>
-            <p>
-                This is a pub component.
-                
-            </p>
+
+
+             <div className="Containerpub">
+                <div className="contentpub">
+                    <div className="pub-Image">
+                        <img src="http://placehold.it/180x150" alt=""/>
+                    </div>
+                    <div className="pub-Content">
+                        <div>
+                            <h2>pub</h2>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+             </div>
+
+
+             
+
+
+             <div className="Containerpub">
+                <div className="contentpub">
+                    <div className="pub-Image">
+                        <img src="http://placehold.it/180x150" alt=""/>
+                    </div>
+                    <div className="pub-Content">
+                        <div>
+                            <h2>pub</h2>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+             </div>
+
+
+           
+
+
+            
+
         </div>
     );
 };
