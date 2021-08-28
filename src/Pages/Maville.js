f923cf8f6090e206dd13948b6ee90e8d1a7f0bce
blacol <anthonycolvil@gmail.com>
2021-08-28 13:11:21 +0200

    dev section maville

--- src/Pages/Maville.js
+++ src/Pages/Maville.js
@@ -5,8 +5,95 @@ const Maville = () => {
     return (
         <div>
             <Navigation/>
-            <h1>Ma ville</h1>
-            
+            <div>
+                <h1>Ma ville</h1>
+                <div className="containermaville">
+
+                    <div className="content1">
+                        <div className="content1g">
+                            <div>
+                                <h1>Titre</h1>
+                            </div>
+
+                            <div>
+                                <p>
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                </p>
+                            </div>
+
+                        </div>
+
+                        <div className="content1d">
+                             <img src="http://placehold.it/600x480" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="spacemaville"></div>
+
+                    
+                    <div className="content1">
+
+                    <div className="content1d">
+                             <img src="http://placehold.it/600x480" alt=""/>
+                        </div>
+
+
+                        <div className="content1g">
+                            <div>
+                                <h1>Titre</h1>
+                            </div>
+
+                            <div>
+                                <p>
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                    Lorem Ipsum est simplement un texte factice de l'industrie de l'impression et de la
+                                    composition. Lorem Ipsum est le texte factice standard de l'industrie depuis les années 1500, 
+                                    lorsqu'un imprimeur inconnu a pris une galère de caractères et l'a brouillé pour en faire un livre 
+                                    spécimen de caractères.
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div> 
         </div>
     );
 };
