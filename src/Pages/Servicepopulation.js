1d9c285ad526b236afc36d5670b4f11d1bc205fb
blacol <anthonycolvil@gmail.com>
2021-09-06 00:19:03 +0200

    ajout sp interface

--- src/Pages/Servicepopulation.js
+++ src/Pages/Servicepopulation.js
@@ -6,6 +6,61 @@ const Servicepopulation = () => {
         <div>
             <Navigation/>
             <h1>Service population</h1>
+            <div className="containerchoixSP">
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 1</h3>
+                </div>
+
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 2</h3>
+                </div>
+
+
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 3</h3>
+                </div>
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 4</h3>
+                </div>
+
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 5</h3>
+                </div>
+
+
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 6</h3>
+                </div>
+
+
+
+                <div className="choixSP">
+                    <div className="News-Image">
+                        <img src="http://placehold.it/300x60" alt=""/>
+                    </div>
+                    <h3>Choisissez votre service 7</h3>
+                </div>
+
+            </div>
+            
             
         </div>
     );
